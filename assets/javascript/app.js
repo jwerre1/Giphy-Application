a5b302130a9df43355aa36bdb06d42f93f504ce6
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-03-08 11:53:25 -0500

    able to change number of gifs that show up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,17 @@
+$(document).ready(function() {  
+
 var tvShows = ["The Office", "Parks and Recreation", "New Girl", "South Park", "Bob's Burgers", "Crazy Ex-Girlfriend", "The Good Place", "Seinfeld", "Friends", "It's Always Sunny In Philadelphia", "The Big Bang Theory", "The Goldbergs", "Community", "Rick and Morty", "Adventure Time", "Archer", "Arrested Development"];
 
+var number = 10;
+
+var tvGifNumber = "";
 
 function displayGifs() {
     $("#gifs-view").empty();
     var tvGif = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvGif + "&api_key=UC1ZPXs6ERCkzy8RRSMRQTEGuv0FMQhf&limit=10";
+    tvGifNumber = tvGif;
+    console.log(tvGifNumber);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvGif + "&api_key=UC1ZPXs6ERCkzy8RRSMRQTEGuv0FMQhf";
     console.log(tvGif);
 
     $.ajax({
@@ -15,7 +22,7 @@ function displayGifs() {
 
         var gifDiv = $("<div>");
 
-        for (i = 0; i < 10; i++) {
+        for (i = 0; i < number; i++) {
 
             var rating = response.data[i].rating;
             var gRating = $("<p>");
@@ -24,6 +31,7 @@ function displayGifs() {
 
             var gGif = $("<img>");
             gGif.attr("src", response.data[i].images.fixed_height.url);
+            gGif.attr("alt", tvGif + " Gif " + (i+1))
             gifDiv.append(gGif);
 
         }
@@ -32,6 +40,47 @@ function displayGifs() {
 
     });
 
+};
+
+function displayEmptyGifs() {
+    $("#gifs-view").empty();
+    var tvGif = tvGifNumber;
+    // tvGifNumber = tvGif;
+    // console.log(tvGifNumber);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvGif + "&api_key=UC1ZPXs6ERCkzy8RRSMRQTEGuv0FMQhf";
+    console.log(tvGif);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+
+        var gifDiv = $("<div>");
+
+        for (i = 0; i < number; i++) {
+
+            var rating = response.data[i].rating;
+            var gRating = $("<p>");
+            gRating.text("Rating: " + rating);
+            gifDiv.append(gRating);
+
+            var gGif = $("<img>");
+            gGif.attr("src", response.data[i].images.fixed_height.url);
+            gGif.attr("alt", tvGif + " Gif " + (i+1))
+            gifDiv.append(gGif);
+
+        }
+
+        $("#gifs-view").prepend(gifDiv);
+
+    });
+
+};
+
+
+function empty() {
+    $("#gifs-view").empty();
 };
 
 
@@ -46,6 +95,12 @@ function genButtons() {
         a.text(tvShows[i]);
         $("#buttons-view").append(a);
     }
+
+    var b = $("<button>");
+    b.addClass("clear btn btn-danger m-1");
+    // a.attr("data-name", tvShows[i]);
+    b.text("Clear");
+    $("#buttons-view").append(b);
 };
 
 $("#add-tv").on("click", function(event){
@@ -58,16 +113,19 @@ $("#add-tv").on("click", function(event){
 
 });
 
+$("#change-numbers").on("click", function(event){
+    event.preventDefault();
+    number = $("#how-many-input").val();
+
+    displayEmptyGifs();
+
+});
+
 $(document).on("click", ".tv", displayGifs);
 
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=friends&api_key=UC1ZPXs6ERCkzy8RRSMRQTEGuv0FMQhf&limit=5";
+$(document).on("click", ".clear", empty);
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response);
-
-// });
 
 genButtons();
+
+});
